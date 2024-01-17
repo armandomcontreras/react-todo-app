ce8fe34fa1675cf43def241023c39b9dbf27173e
Armando <amcontreras10@gmail.com>
2024-01-17 11:10:28 -0500

    declaring functions throughout the pages

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
 
   const addTodo = todo => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
-      return
+      return;
     }
-    const newTodos = [todo, ...todos]
+    const newTodos = [todo, ...todos];
 
-    setTodos(newTodos)
-    console.log(...todos);
+    setTodos(newTodos);
   };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete
+      }
+      return todo
+    })
+    setTodos(updatedTodos);
+  }
     
-    return (
+  return (
     <div>
-            <h1>What's the Plan for Today?</h1> 
-        <TodoForm onSubmit={addTodo} />
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodos={completeTodo}
+      />
             
     </div>
-  )
+  );
 }
 
 export default TodoList;
