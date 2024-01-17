1526c1498b0a2158dc2d1827b626f1d71abe7595
Armando <amcontreras10@gmail.com>
2024-01-17 10:54:20 -0500

    TodoList updates

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,24 @@ import TodoForm from './TodoForm';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+    const newTodos = [todo, ...todos]
+
+    setTodos(newTodos)
+    console.log(...todos);
+  };
     
     return (
     <div>
             <h1>What's the Plan for Today?</h1> 
-            <TodoForm/>
+        <TodoForm onSubmit={addTodo} />
             
     </div>
   )
 }
 
-export default TodoList
+export default TodoList;
